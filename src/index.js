9bf460e3bd0e7f189055150fb696fd1c0786fcee
xujian <2513573453@qq.com>
2021-09-12 00:01:45 +0800

    feat:引入重置相关样式

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// 引入重置css样式
+import "@/assets/css/reset.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
